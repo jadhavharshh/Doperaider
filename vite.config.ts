345c3a80797b08c616140e4c26b03b43c8ff8985
jadhavharshh <harshjadhavconnect@gmail.com>
2025-06-16 00:49:26 +0530

    Installed tailwind css

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
+import tailwindcss from '@tailwindcss/vite'
 import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
+  plugins: [react(), mode === "development" && screenGraphPlugin(), tailwindcss()],
   publicDir: "./static",
   base: "./",
 }));
